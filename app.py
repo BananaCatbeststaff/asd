77208eeedec5935e108f29f83225ddeda2f7b88e
Founder_on <galinha.chata.muito.chata@gmail.com>
2025-10-15 14:11:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# file: app.py
 from fastapi import FastAPI, Response
 
 app = FastAPI()
@@ -7,7 +6,5 @@ KEY_VALUE = "A12fjkmajçop3DSFS"
 
 @app.get("/keys")
 def get_keys_raw():
-    # Retorna texto simples (raw) com encoding UTF-8
+    # Retorna texto puro (sem JSON)
     return Response(content=KEY_VALUE, media_type="text/plain; charset=utf-8")
-
-
