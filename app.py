c93099c1b5d1c08a9b8bb397ab94436c9c89c802
Founder_on <galinha.chata.muito.chata@gmail.com>
2025-10-15 14:13:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, Response
 
 app = FastAPI()
 
-KEY_VALUE = "A12fjkmajçop3DSFS"
+KEYS = ["A12fjkmajçop3DSFS"]
 
 @app.get("/keys")
 def get_keys_raw():
-    # Retorna texto puro (sem JSON)
-    return Response(content=KEY_VALUE, media_type="text/plain; charset=utf-8")
+    content = "\n".join(KEYS)
+    return Response(content=content.strip(), media_type="text/plain; charset=utf-8")
